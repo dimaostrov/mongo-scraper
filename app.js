36347142eba113d43da671c91f9f3d0d6af568f2
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-07-08 22:56:32 -0700

    add api

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
+import express from 'express';
+import mongoose from 'mongoose';
+import path  from'path';
+import exphbs  from 'express-handlebars';
+import cookieParser  from 'cookie-parser';
+import bodyParser  from 'body-parser';
+import logger  from 'morgan';
 
-const indexRouter = require('./routes/index');
+import indexRouter from './routes/index';
+import apiRouter from './routes/api';
 
-var app = express();
+const app = express();
 mongoose.connect('mongodb://localhost:27017/mongoscraper');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -35,7 +36,7 @@ app.use(function(req, res, next) {
 })
 
 app.use('/', indexRouter);
-
+app.use('/api', apiRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -44,12 +45,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message  err.message;
+  res.locals.error  req.app.get('env')  'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+module.exports  app;
