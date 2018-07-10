6e0aa21194e6c73494f7006d9f96f6f9486a18be
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-07-09 19:03:43 -0700

    mess around with promises

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -2,7 +2,6 @@ const promise = require("../scripts/scrape");
 const request = require("request");
 const { Headline } = require("../models");
 
-
 const fetchResults = (req, res) => {
   promise.then(x => {
     x.map(article => insertToDb(article));
@@ -14,54 +13,72 @@ const fetchResults = (req, res) => {
 // and populates new articles into db
 // via scrape.
 const homepage = (req, res, next) => {
-  getArticles.then(posts => {
-    res.render("index", { posts });
-  })
-  .catch(err=> console.log(err));
-  scrapeArticles.then(posts => {
-    posts.map(post => insertToDb(post));
-  }).catch(err=>console.log(err))
+  getArticles()
+    .then(posts => {
+      res.render("index", { posts });
+    })
+    .catch(err => console.log(err));
+  scrapeArticles()
+    .then(posts => {
+      posts.map(post => insertToDb(post));
+    })
+    .catch(err => console.log(err));
+
+  };
+
+const insertToDb = article => {
+  Headline.create(
+    {
+      title: article.title,
+      image: article.image,
+      link: article.link,
+      author: article.author
+    },
+    function(err, article) {
+      if (err) return err;
+      console.log(`${article.title.split(0, 10)} has been saved`);
+    }
+  );
 };
 
-const insertToDb = (article) => {
-  Headline.create({ title: article.title, image: article.image, link: article.link, author: article.author }, function(err, article){
-    if(err) return err;
-    console.log(`${article.title.split(0, 10)} has been saved`);
-  });
-}
-
 // get articles from the db
-const getArticles = new Promise((resolve, reject) => {
-  request(
-    {
-      method: "GET",
-      url: "http://127.0.0.1:3000/getfromDB"
-    },
-    function(err, response, body, callback) {
-      if (err) reject(err);
+const getArticles = () => {
+  let action = new Promise((resolve, reject) => {
+    request(
+      {
+        method: "GET",
+        url: "http://127.0.0.1:3000/getfromDB"
+      },
+      function(err, response, body, callback) {
+        if (err) reject(err);
 
-      resolve(JSON.parse(body));
-    }
-  );
-});
+        resolve(JSON.parse(body));
+      }
+    );
+  });
+  return action;
+};
 
 // scrape articles
-const scrapeArticles = new Promise((resolve, reject) => {
-  request(
-    {
-      method: "GET",
-      url: "http://127.0.0.1:3000/scrape"
-    },
-    function(err, response, body, callback) {
-      if (err) reject(err);
+const scrapeArticles = () => {
+  let action = new Promise((resolve, reject) => {
+    request(
+      {
+        method: "GET",
+        url: "http://127.0.0.1:3000/scrape"
+      },
+      function(err, response, body, callback) {
+        if (err) reject(err);
 
-      resolve(JSON.parse(body));
-    }
-  );
-});
+        resolve(JSON.parse(body));
+      }
+    );
+  });
+  return action;
+};
 
 module.exports = {
   fetchResults,
   homepage,
   insertToDb
-}
+};
