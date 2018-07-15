9aecca9dc6c2b44246665a378f1e1242679c6a82
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-07-15 14:18:42 -0700

    Add basic template

---
+++ public/assets/js/savedarticles.js
@@ -0,0 +1,41 @@
+const articles = document.getElementById("articles");
+
+var Article = {
+  view: function() {
+    return m("article", { class: "bt bb b--black-10"}, [
+      m("div", { class: "db pv4 ph3 ph0-l no-underline black" }, [
+        m('div', {class: "flex flex-column flex-row-ns"}, [
+          m('div', {class: "pr3-ns mb4 mb0-ns w-100 w-40-ns"}, [
+            m('a', {href: '/'}, [
+              m('img', {src: '/', class: "db", alt: "title"})
+            ])
+          ]),
+          m('div', {class: "w-100 w-60-ns pl3-ns"}, [
+            m('a', {class: "link"}, [
+              m('h1', {class: "link f3 fw1 baskerville mt0 lh-title"}, 'title')
+            ]),
+            m('p', {class: "f6 lh-copy mv0"}, "By author"),
+            m('button', {class: " butn save"}, 'Delete Article'),
+            m('div', "notes")
+          ])
+        ])
+      ])
+    ]);
+  }
+};
+
+var count = 0
+var increment = function() {
+    m.request({
+        method: "PUT",
+        url: "//rem-rest-api.herokuapp.com/api/tutorial/1",
+        data: {count: count + 1},
+        withCredentials: true,
+    })
+    .then(function(data) {
+        count = parseInt(data.count)
+    })
+}
+
+m.mount(articles, Article);
+
